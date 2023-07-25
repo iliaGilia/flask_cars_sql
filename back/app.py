6cfd0e19703e7dacf386aee6e2e0e3278b82a46b
iliaGilia <127669483+iliaGilia@users.noreply.github.com>
2023-07-25 12:21:42 +0300

    Add files via upload

--- back/app.py
+++ back/app.py
@@ -4,7 +4,7 @@ import mysql.connector
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'PASSWORD'
+app.config['MYSQL_PASSWORD'] = 'iliA4122002'
 app.config['MYSQL_DB'] = 'car_database'
 
 mysql = mysql.connector.connect(
